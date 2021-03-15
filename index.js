666b18fb0867a3d7c6aa61af6dd21dcd5c7549de
lazarusWist <lawrence.witt@hotmail.co.uk>
2021-03-15 12:14:55 +0000

    change setState to use ref.current and initialise ref with state

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var React = require('react');
 
 module.exports = function useStateRef(defaultValue) {
   var [state, setState] = React.useState(defaultValue);
-  var ref = React.useRef(defaultValue);
+  var ref = React.useRef(state);
 
   var dispatch = React.useCallback(function(val) {
     ref.current = typeof val === "function" ?
     val(ref.current) : val;
 
-    setState(val);
+    setState(ref.current);
   }, []);
 
   return [state, dispatch, ref];
