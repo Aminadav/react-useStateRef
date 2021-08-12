c5636e6ef1e6ea41159c8af10d7ba765b9043e90
Louis-Marie Michelin <lmichelin@outlook.fr>
2021-08-12 12:58:10 +0200

    use better syntax for TS function overloading

--- index.ts
+++ index.ts
@@ -3,27 +3,22 @@ import { useCallback, useRef, useState, SetStateAction, MutableRefObject, Dispat
 const isFunction = <S>(setStateAction: SetStateAction<S>): setStateAction is (prevState: S) => S =>
   typeof setStateAction === "function";
 
-function useStateRef<S>(
-  initialState: S | (() => S)
-): [S, Dispatch<SetStateAction<S>>, MutableRefObject<S>];
+type UseStateRef = {
+  <S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>, MutableRefObject<S>];
+  <S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>, MutableRefObject<S | undefined>];
+};
 
-function useStateRef<S = undefined>(): [
-  S | undefined,
-  Dispatch<SetStateAction<S | undefined>>,
-  MutableRefObject<S | undefined>
-];
-
-function useStateRef<S>(initialState?: S | (() => S)) {
+const useStateRef: UseStateRef = <S>(initialState?: S | (() => S)) => {
   const [state, setState] = useState(initialState);
   const ref = useRef(state);
 
-  const dispatch: Dispatch<SetStateAction<S | undefined>> = useCallback(setStateAction => {
+  const dispatch: typeof setState = useCallback(setStateAction => {
     ref.current = isFunction(setStateAction) ? setStateAction(ref.current) : setStateAction;
 
     setState(ref.current);
   }, []);
 
   return [state, dispatch, ref];
-}
+};
 
 export = useStateRef;
