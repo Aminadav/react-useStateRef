d1c18f002f830f77dced55c858be9a1088fb5d73
Louis-Marie Michelin <lmichelin@outlook.fr>
2021-10-08 23:16:38 +0200

    fix: make ref.current readonly (#8)

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
-import { useCallback, useRef, useState, SetStateAction, MutableRefObject, Dispatch } from "react";
+import { useCallback, useRef, useState, SetStateAction, Dispatch } from "react";
 
 const isFunction = <S>(setStateAction: SetStateAction<S>): setStateAction is (prevState: S) => S =>
   typeof setStateAction === "function";
 
+type ReadOnlyRefObject<T> = {
+  readonly current: T;
+};
+
 type UseStateRef = {
-  <S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>, MutableRefObject<S>];
-  <S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>, MutableRefObject<S | undefined>];
+  <S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>, ReadOnlyRefObject<S>];
+  <S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>, ReadOnlyRefObject<S | undefined>];
 };
 
 const useStateRef: UseStateRef = <S>(initialState?: S | (() => S)) => {
